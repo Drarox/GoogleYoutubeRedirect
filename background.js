7de261355202ca2974a4e655153fb3b020ed0ed6
Yannick <yannick.b443@gmail.com>
2023-04-09 19:20:58 +0200

    Multi tab handled

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-let prevUrl = '';
+const prevUrls = [];
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.url && tab.url.includes('google.com/search') && tab.url.includes('#fpstate=ive')) {
@@ -8,14 +8,16 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (vidParam) {
       const youtubeUrl = "https://www.youtube.com/watch?v=" + vidParam;
       const googleUrl = url.href.split('#fpstate=ive')[0];
-      
+
+      const currentUrlIndex = prevUrls.indexOf(tab.url);
+
       if (!tab.active) { //Redirect if opened in a new tab
           chrome.tabs.update(tabId, {url: youtubeUrl});
-      } else if (tab.url !== prevUrl) { //Redirect only if has not already 
-          prevUrl = tab.url;
+      } else if (currentUrlIndex == -1) { //Redirect only if has not already 
+          prevUrls.push(tab.url);
           chrome.tabs.update(tabId, {url: youtubeUrl});
       } else { //Reset to original google url when back button is pressed
-          prevUrl = '';
+          prevUrls.splice(currentUrlIndex, 1);
           chrome.tabs.update(tabId, {url: googleUrl});
       }
     }
